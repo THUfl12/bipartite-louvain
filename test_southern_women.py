3f87215f34ae8cbe711f3a15149b8bcce0d02dca
LiangFeng <1027555983@qq.com>
2019-10-08 15:14:02 +0800

    Update test_southern_women.py

--- test_southern_women.py
+++ test_southern_women.py
@@ -9,10 +9,9 @@ import networkx as nx
 from time import time
 from src.new_bilouvain import new_bilouvain
 from src.bigraph import BiGraph
-from networkx.algorithms import bipartite
 import numpy as np
 
-file = '../data/southern_women/edges.txt'
+file = 'edges.txt'
 
 df = pd.read_csv(file, header=None, sep=' ')
 df.columns = ['women', 'event']
@@ -34,12 +33,6 @@ G.add_nodes_from(unique_event, color='blue')
 
 for i in range(len(women_list)):
     G.add_edge(women_list[i], event_list[i]+women_number)
-
-adjacent_matrix = bipartite.biadjacency_matrix(G, unique_women, unique_event)
-bi_adjacent = adjacent_matrix.todense()
-
-file_mat = 'data.txt'
-np.savetxt(file_mat, bi_adjacent, fmt='%d', delimiter=' ')
 
 epochs = 20
 start_time = time()
